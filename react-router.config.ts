eac4c6676327bd8f28397cce6d878d25e60255b5
zaenal-blip <arif4321456789@gmail.com>
2026-04-29 20:08:04 +0700

    fix: fix deploy

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  basename: "/rentivo-fe",
+  // basename: "/rentivo-fe",
+
 } satisfies Config;
